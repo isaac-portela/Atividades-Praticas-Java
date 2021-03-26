b69c00d435d6dabc488f3938fbb1ce595aaf2b25
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-26 16:37:35 -0300

    Add files via upload

---
+++ JavaServer/AplicacaoWeb/src/java/IsaacPortela.java
@@ -0,0 +1,86 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author isaac
+ */
+@WebServlet(urlPatterns = {"/IsaacPortela"})
+public class IsaacPortela extends HttpServlet {
+
+ 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            Calendar data = Calendar.getInstance();
+            int hora = data.get(Calendar.HOUR_OF_DAY);
+            String nome = request.getParameter("meuNome");
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet IsaacPortela</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Olá " + nome + "!</h1>");
+            if (hora < 12) 
+                out.println("<h1> Bom dia!</h1>");
+             else if (hora < 18) 
+                out.println("<h1> Boa Tarde!</h1>");
+            else
+                 out.println("<h1> Boa Noite!</h1>");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
