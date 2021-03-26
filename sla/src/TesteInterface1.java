58fe542c2ce33621974b7ffc2e54497e84f43b42
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-26 16:35:34 -0300

    Delete sla directory

--- sla/src/TesteInterface1.java
+++
@@ -1,7 +0,0 @@
-public class TesteInterface1 {
-    public static void main(String[] args) {
-        Interface1 interfa = new Interface1();
-        interfa.setDefaultCloseOperation(interfa.EXIT_ON_CLOSE);
-        interfa.setVisible(true);
-    }
-}
