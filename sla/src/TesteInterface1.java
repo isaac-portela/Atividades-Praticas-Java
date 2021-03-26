be14726e04e5fabbe0a2544aa1e141d240703d9a
kroos <54759116+KroosBR@users.noreply.github.com>
2021-03-26 16:34:25 -0300

    Add files via upload

---
+++ sla/src/TesteInterface1.java
@@ -0,0 +1,7 @@
+public class TesteInterface1 {
+    public static void main(String[] args) {
+        Interface1 interfa = new Interface1();
+        interfa.setDefaultCloseOperation(interfa.EXIT_ON_CLOSE);
+        interfa.setVisible(true);
+    }
+}
