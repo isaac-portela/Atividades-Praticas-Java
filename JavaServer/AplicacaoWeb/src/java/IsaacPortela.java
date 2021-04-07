c353c406114a54b327834393e7b8876dbeae370d
kroos <54759116+KroosBR@users.noreply.github.com>
2021-04-07 02:24:34 -0300

    Add files via upload

--- JavaServer/AplicacaoWeb/src/java/IsaacPortela.java
+++ JavaServer/AplicacaoWeb/src/java/IsaacPortela.java
@@ -15,7 +15,15 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = {"/IsaacPortela"})
 public class IsaacPortela extends HttpServlet {
 
- 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
